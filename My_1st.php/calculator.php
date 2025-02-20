a03d9da78bc7ed4249bf6c9026f6810cdbc93f68
cytormod <sahil.gopani5@gmail.com>
2025-02-20 11:36:11 +0100

    Update calculator.php

--- My_1st.php/calculator.php
+++ My_1st.php/calculator.php
@@ -8,14 +8,14 @@
 </head>
 <body>
     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-        <input type="number" name="'num01" placeholder="Number one">
+        <input type="number" name="num01" placeholder="Number one">
         <select name="operator" id="">
             <option value="add">+</option>
             <option value="subtract">-</option>
             <option value="multiply">*</option>
             <option value="divide">/</option>
         </select>
-        <input type="number" name="'num02" placeholder="Number two">
+        <input type="number" name="num02" placeholder="Number two">
 
         <button>Calculate</button>
 
@@ -38,7 +38,7 @@
             $errors = true;
         }
 
-        if (is_numeric($num01) || is_numeric($num02)) {
+        if (!is_numeric($num01) || !is_numeric($num02)) {
             echo "Only Write Numbers";
             $errors = true;
         }
